e7d08e3e42cd76530c1d9d01f72b4c8dce8cda03
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-06-19 15:47:03 +0200

    test4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  const express = require('express')
  const app = express()
 const bodyParser = require('body-parser')
-app.use(express.static('public'))
+//app.use(express.static('public'))
 app.use(bodyParser.json())
 
 sequelize.sync() .then(() => {
@@ -74,5 +74,5 @@ app.get('/team_info/:id', (req, res, next) => {
   })
   .catch(error => next(error))
 })
- const port = 5000
+ const port =process.env.PORT|| 5000
  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
