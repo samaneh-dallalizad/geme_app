4018dd21447842bdfc20d217199350b9547ceb92
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-06-19 16:16:57 +0200

    test7

--- index.js
+++ index.js
@@ -78,13 +78,13 @@ app.get('/team_info/:id', (req, res, next) => {
       .then(team => {
         console.log(team)
       })
-      .catch(error => console.log(error) //next(error))
+      .catch(error => console.log(error) )
     }
     else{
       res.json(team)
     }
   })
-  .catch(error => console.log(error) //next(error))
+  .catch(error => console.log(error))
 })
  const port =process.env.PORT|| 5000
  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
